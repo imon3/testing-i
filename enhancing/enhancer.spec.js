08cadf0a4fbc4154534db557a4f65da205f8cafc
Imon Ovbude <imonovbude@gmail.com>
2019-03-11 12:58:02 -0700

    completed function repair to return durability 100. wrote test to check if durability will return 100

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,11 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('eenhancer.js', () => {
+    it('should restore durability to 100', () => {
+        const item = { durability: 55 };
+        const repair = enhancer.repair(item);
+
+        expect(repair.durability).toBe(100);
+    });
+});
